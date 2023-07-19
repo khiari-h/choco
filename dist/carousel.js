d5ad76258359988949cb262e2a6878fbd4cb28eb
Hamdane Khiari <hamdane.khiari@gmail.com>
2023-07-19 23:35:03 +0200

    a

---
+++ dist/carousel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+function Carouselaccueil() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement(Carousel, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("img", {
+    src: "images/accueil1.jpg",
+    alt: "Accueil 1"
+  }), /*#__PURE__*/React.createElement("p", {
+    className: "legend"
+  }, "L\xE9gende de l'image 1")), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("img", {
+    src: "images/accueil2.jpg",
+    alt: "Accueil 2"
+  }), /*#__PURE__*/React.createElement("p", {
+    className: "legend"
+  }, "L\xE9gende de l'image 2")), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("img", {
+    src: "images/accueil3.jpg",
+    alt: "Accueil 3"
+  }), /*#__PURE__*/React.createElement("p", {
+    className: "legend"
+  }, "L\xE9gende de l'image 3"))));
+}
+export default Carouselaccueil;
