d36c09f9c577973244d7a24ecff82deb399d0aa4
Hamdane Khiari <hamdane.khiari@gmail.com>
2023-07-19 22:18:49 +0200

    maj3

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Importer les dépendances React et ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Importer le composant racine de votre application
-
 import Carouselaccueil from './carousel';
 
 // Montez le composant racine dans le DOM
